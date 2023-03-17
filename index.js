a0c65ec6784124f1b06442f9e9ec8da4e055b104
Aarif-shazZ <shazaarif0@gmail.com>
2023-03-17 18:21:19 +0530

    backend Deploy

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const {userRoutes} = require("../Backend/routes/User.route")
 const {productRoutes} = require("../Backend/routes/Product.route")
 
 const app  = express()
-app.use(cors())
+app.use(cors({
+    origin:"*"
+}))
 
 app.use(express.json())
 app.get("/",(req,res) => {
